d6db488f88ab8f1fa3147729b28d31e3853ba02d
Ahmed-Badawy <me@ahmed-badawy.com>
2016-10-25 17:06:44 +0200

    minor update to langu

--- 16-10-2016-LanguHelper/index.js
+++ 16-10-2016-LanguHelper/index.js
@@ -1,32 +1,40 @@
+let $options = {
+	files : [
+		"index.html",
+		"index2.html"
+	],
+	output_dir : "./build",
+	langs: ["-en","-ar","-sp"]
+};
+
+
 var fs = require('fs');
 
+$options.files.forEach(function(file){
+	console.log("----------------------------------Calling Incs----------------------------------");
+	let contents = fs.readFileSync(file, 'utf8');
+		let includes = function(contents){
+			return contents.replace(/inc\[\[\[.*\]\]\]/ig, value=> fs.readFileSync(value.replace(/inc\[\[\[|\]\]\]/ig,''),'utf8') );
+		}
+		for(let i=0;i<=25;i++) contents = includes(contents);
 
-console.log("----------------------------------Calling Incs----------------------------------");
-let contents = fs.readFileSync('index.html', 'utf8');
-	contents = contents.replace(/inc\[\[\[.*\]\]\]/ig, value=> fs.readFileSync(value.replace(/inc\[\[\[|\]\]\]/ig,''),'utf8') );
-/**********************************************************************/
+	/**********************************************************************/
 
+	console.log("----------------------------------replacing the tt functions----------------------------------");
+	function replace_contents(contents,lang_index){
+		return contents.replace( /tt\[\[\[.*\]\]\]/ig, value=> {
+			let langs_array = value.replace(/tt\[\[\[|\]\]\]/ig,'').split('###');
+			return langs_array[lang_index] || langs_array[0];
+		});
+	}
+	let lagns_content = [];
+	$options.langs.forEach( (lang,i)=> lagns_content.push(replace_contents(contents,i)) );
+	/**********************************************************************/
 
-console.log("----------------------------------replacing the tt functions----------------------------------");
-function replace_contents(contents,lang_index){
-	return contents.replace( /tt\[\[\[.*\]\]\]/ig, value=> value.replace(/tt\[\[\[|\]\]\]/ig,'').split('###')[lang_index] )
-}
-
-let lang1_contents = replace_contents(contents,0);
-let lang2_contents = replace_contents(contents,1);
-/**********************************************************************/
-
-
-console.log("----------------------------------Creating the new files----------------------------------");
-fs.writeFileSync('index-en.html',lang1_contents);
-fs.writeFileSync('index-ar.html',lang2_contents);
-// /**********************************************************************/
-
-
-
-
-
-
-
-
-
+	console.log("----------------------------------Creating the new files----------------------------------");
+	let file_array = file.split('.');
+	let ext = file_array.pop();
+	file_array.join('.');
+	lagns_content.forEach( (content,i)=> fs.writeFileSync($options.output_dir+"/"+file_array+`${$options.langs[i]}.${ext}`,content) );
+	// /**********************************************************************/	
+})
